fa10c0cf1bb7370a4ad5afeaf5fab463c54d52d1
Thiago Pereira <falecomthiagopereira@gmail.com>
2023-04-18 15:04:56 -0300

    User Profile is showing

---
+++ src/components/Search.tsx
@@ -0,0 +1,29 @@
+import React, { useState, KeyboardEvent } from 'react';
+import { BsSearch } from 'react-icons/bs'
+import { SearchProps } from '../types/search';
+
+const Search = ({loadUser}: SearchProps) => {
+
+  const [userName, setUserName] = useState("");
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      loadUser(userName);
+    }
+  };
+
+  return (
+    <div className='search'>
+      <h2>Search User:</h2>
+      <p>Discovery your best repositories</p>
+      <div className='search_container'>
+        <input type="text" placeholder='Type username from github' onChange={(e) => setUserName(e.target.value)} onKeyDown={handleKeyDown} />
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch />  
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
